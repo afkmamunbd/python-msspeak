3c6e5d6e7c1de8b06ba37b3747ed55e0fc01f557
afkmamunbd <afkmamunbd@engineer.com>
2019-09-01 02:03:26 +0000

    Update to version v

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='python-msspeak',
-    version='0.5.0',
+    version='',
     description="Text-To-Speech with MSSpeak",
     long_description=readme + '\n\n' + changelog,
     author="Areski Belaid",
